a79053f0e9179a039f27e5cded01f8b3d9894961
lokified <sheldon.obonyo@student.moringaschool.com>
2022-01-20 11:47:53 +0300

    add new properties for the session time and name of session

--- src/main/java/models/Session.java
+++ src/main/java/models/Session.java
@@ -1,20 +1,49 @@
 package models;
 
 import java.sql.Timestamp;
+import java.util.Objects;
 
 public class Session {
     private int id;
+    private String sessionName;
     private String invitationLink;
-    private Timestamp sessionTime;
+    private Timestamp startTime;
+    private Timestamp endTime;
     private String description;
 
     private String type;
 
-    public Session(String invitationLink, Timestamp sessionTime, String description, String type) {
+    public Session(String sessionName, String invitationLink, Timestamp startTime, Timestamp endTime, String description, String type) {
+        this.sessionName = sessionName;
         this.invitationLink = invitationLink;
-        this.sessionTime = sessionTime;
+        this.startTime = startTime;
+        this.endTime  =endTime;
         this.description = description;
         this.type = type;
+    }
+
+    public String getSessionName() {
+        return sessionName;
+    }
+
+    public void setSessionName(String sessionName) {
+        this.sessionName = sessionName;
+    }
+
+    public Timestamp getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Timestamp startTime) {
+        this.startTime = startTime;
+    }
+
+    public Timestamp getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Timestamp endTime) {
+        this.endTime = endTime;
     }
 
     public int getId() {
@@ -29,16 +58,21 @@ public class Session {
         return invitationLink;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Session session = (Session) o;
+        return id == session.id && sessionName.equals(session.sessionName) && invitationLink.equals(session.invitationLink) && startTime.equals(session.startTime) && endTime.equals(session.endTime) && description.equals(session.description) && type.equals(session.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, sessionName, invitationLink, startTime, endTime, description, type);
+    }
+
     public void setInvitationLink(String invitationLink) {
         this.invitationLink = invitationLink;
-    }
-
-    public Timestamp getSessionTime() {
-        return sessionTime;
-    }
-
-    public void setSessionTime(Timestamp sessionTime) {
-        this.sessionTime = sessionTime;
     }
 
     public String getDescription() {
@@ -57,27 +91,5 @@ public class Session {
         this.type = type;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
 
-        Session session = (Session) o;
-
-        if (id != session.id) return false;
-        if (!invitationLink.equals(session.invitationLink)) return false;
-        if (!sessionTime.equals(session.sessionTime)) return false;
-        if (!description.equals(session.description)) return false;
-        return type.equals(session.type);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + invitationLink.hashCode();
-        result = 31 * result + sessionTime.hashCode();
-        result = 31 * result + description.hashCode();
-        result = 31 * result + type.hashCode();
-        return result;
-    }
 }
